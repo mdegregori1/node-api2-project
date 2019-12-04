7d4f422b1ea3196289fd36aed4da46008dadc209
Mauricio Degregori <degregori2@gmail.com>
2019-12-03 21:49:07 -0600

    get /:id/comments

--- data/posts-router.js
+++ data/posts-router.js
@@ -52,4 +52,30 @@ router.get('/:id', (req, res) => {
     })
 })
 
+//GET	/api/posts/:id/comments
+router.get('/:id/comments', (req, res) => {
+    const id = req.params.id;
+    db.findPostComments(id)
+    .then( id => {
+        if (id) {
+            res.status(200).json(id)
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({ error: "The comments information could not be retrieved." })
+    })
+})
+
+// If the post with the specified id is not found:
+
+// return HTTP status code 404 (Not Found).
+// return the following JSON object: { message: "The post with the specified ID does not exist." }.
+// If there's an error in retrieving the comments from the database:
+
+// cancel the request.
+// respond with HTTP status code 500.
+// return the following JSON object: { error: "The comments information could not be retrieved." }.
 module.exports = router;
