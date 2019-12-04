702a404574b04612055ae670a6e10427e9e35b28
Mauricio Degregori <degregori2@gmail.com>
2019-12-04 01:38:21 -0600

    erased commented out stuff on mvp

--- api/server.js
+++ api/server.js
@@ -19,25 +19,3 @@ server.use('/api/posts', postsRouter)
 
 module.exports = server;
 
-// const express = require('express');
-
-// const hubsRouter = require('../hubs/hubs-router.js');
-
-// const server = express();
-
-// server.use(express.json());
-
-// server.get('/', (req, res) => {
-//   res.send(`
-//     <h2>Lambda Hubs API</h>
-//     <p>Welcome to the Lambda Hubs API</p>
-//   `);
-// });
-
-// //only USE (.use) if matches path
-// //when you wanna use x (below), use hubsRouter
-// server.use('/api/hubs', hubsRouter)
-
-
-// // export default server;
-// module.exports = server;
