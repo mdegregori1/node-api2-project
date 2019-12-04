00f5cdd3abb7dad4934c2e2ea59e23c43e8858a6
Mauricio Degregori <degregori2@gmail.com>
2019-12-03 19:55:56 -0600

    get by id functioning

--- data/posts-router.js
+++ data/posts-router.js
@@ -35,4 +35,21 @@ router.get('/', (req, res) => {
     })
 })
 
+// GET	/api/posts/:id
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    db.findById(id)
+    .then( id => {
+        if (id) {
+            res.status(200).json(id)
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    })
+    .catch( error => {
+        console.log(error);
+        res.status(500).json( { error: "The post information could not be retrieved." })
+    })
+})
+
 module.exports = router;
